f835158fa2241a42e2da7b9a9bc61da3487581fa
r-scott25 <rachelbernal2@gmail.com>
2022-11-03 14:40:41 -0500

    objects

---
+++ objects/objects.js
@@ -0,0 +1,22 @@
+//  **** Objects *****
+
+let person = {
+  name: "Rachel",
+  age: 41,
+};
+
+console.log(person);
+
+//  Access properties in objects using Dot Notation or Bracket Notation
+
+// Dot Notation
+person.name = "Lynn";
+
+console.log(person);
+
+console.log(person.name);
+
+// Bracket Notation
+person["name"] = "Scott";
+
+console.log(person.name);
