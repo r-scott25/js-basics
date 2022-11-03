767bd3961e74fe4703bc2c7e1d1b21ce6cc42412
r-scott25 <rachelbernal2@gmail.com>
2022-11-03 14:29:54 -0500

    primitive types

---
+++ primitive-types/primitive-types.js
@@ -0,0 +1,23 @@
+// ****** Primitive/Value Types
+
+// String;
+// Number;
+// Boolean;
+// undefined;
+// null;
+
+// String Literal
+let name = "Rachel";
+
+// Number Literal
+let age = 30;
+
+// Boolean Literal
+let isApproved = true;
+
+// Undefined
+let firstName;
+let middleName = undefined;
+
+// Null - use when clearing the value of a variable
+let selectedColor = null;
