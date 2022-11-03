b4f58efd00b07d410d67a6954062a4a190c00fab
r-scott25 <rachelbernal2@gmail.com>
2022-11-03 15:39:38 -0500

    functions

---
+++ functions/functions.js
@@ -0,0 +1,21 @@
+//  ****** Functions ******
+
+// Functions to Perform a Task:
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+greet("Rachel");
+
+function greet2(firstName, lastName) {
+  console.log("Hello " + firstName + " " + lastName);
+}
+greet2("Rachel", "Scott");
+
+// Functions to Calculate Something:
+
+function square(number) {
+  return number * number;
+}
+
+console.log(square(2));
