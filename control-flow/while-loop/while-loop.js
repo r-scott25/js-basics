c9c7b45990afb6695fdfe1b30aeed154d81a5f08
r-scott25 <rachelbernal2@gmail.com>
2022-11-07 13:13:29 -0600

    while loop

---
+++ control-flow/while-loop/while-loop.js
@@ -0,0 +1,18 @@
+// While Loop
+
+// Difference between a for loop and a while loop:
+// In a for loop, the loop variable is part of the loop variable itself.
+// In a while loop, you have to declare this while variable externally.
+
+// let variable = value;
+// while(condition) {
+//   statement
+// }
+
+let i = 0;
+while (i <= 5) {
+  if (i % 2 !== 0) {
+    console.log(i);
+    i++;
+  }
+}
