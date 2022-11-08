8dc1ff039bf374e28ec7a7424863902af787ace3
r-scott25 <rachelbernal2@gmail.com>
2022-11-08 13:09:37 -0600

    infinite loops

---
+++ control-flow/infinite-loop/infinite-loop.js
@@ -0,0 +1,13 @@
+//  Infinite Loops
+
+// Infinite loops will crash your browser or computer - avoid them
+
+// *** EXAMPLE *** //
+
+// let i = 0
+// while ( i < 5)
+// console.log(i);
+
+// The above will always be 0, so will continue running for ever
+
+// There are also Infinite Do-while loops and For loops
